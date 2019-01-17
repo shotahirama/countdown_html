af489a125b6021489794172a359c341d2e685773
shotahirama <vghjjj@gmail.com>
2019-01-18 08:48:46 +0900

    add yaml edit page

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sort"
+	"time"
 
 	flags "github.com/jessevdk/go-flags"
 	yaml "gopkg.in/yaml.v2"
@@ -27,29 +29,36 @@ type Options struct {
 
 var opts Options
 
-func htmlHandler(w http.ResponseWriter, req *http.Request) {
+func Exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
 
-	buf, err := ioutil.ReadFile("countdown.yaml")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	var d CountDownData
-	err = yaml.Unmarshal(buf, &d)
-	if err != nil {
-		log.Fatalln(err)
-	}
+func htmlHandler(w http.ResponseWriter, req *http.Request) {
 	t := template.Must(template.ParseFiles("./resources/index.html"))
 
 	var s []map[string]string
-	for _, value := range d.CountDowns {
-		v := url.Values{}
-		v.Set("name", value.Name)
-		v.Set("timelimit", value.Date)
-		sa := map[string]string{
-			"name": value.Name,
-			"URL":  "/countdown" + "?" + v.Encode(),
+	if Exists("countdown.yaml") {
+		buf, err := ioutil.ReadFile("countdown.yaml")
+		if err != nil {
+			log.Fatalln(err)
 		}
-		s = append(s, sa)
+		var d CountDownData
+		err = yaml.Unmarshal(buf, &d)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, value := range d.CountDowns {
+			v := url.Values{}
+			v.Set("name", value.Name)
+			v.Set("timelimit", value.Date)
+			sa := map[string]string{
+				"name": value.Name,
+				"URL":  "/countdown" + "?" + v.Encode(),
+			}
+			s = append(s, sa)
+		}
 	}
 
 	if err := t.ExecuteTemplate(w, "index.html", s); err != nil {
@@ -65,6 +74,67 @@ func countdownHandeler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func yamleditHandeler(w http.ResponseWriter, req *http.Request) {
+	t := template.Must(template.ParseFiles("./resources/yamledit.html"))
+
+	var s []map[string]string
+	if Exists("countdown.yaml") {
+		buf, err := ioutil.ReadFile("countdown.yaml")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var d CountDownData
+		err = yaml.Unmarshal(buf, &d)
+		for _, value := range d.CountDowns {
+			sa := map[string]string{
+				"name": value.Name,
+				"date": value.Date,
+			}
+			s = append(s, sa)
+		}
+	}
+
+	if err := t.ExecuteTemplate(w, "yamledit.html", s); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type CountDowns []CountDown
+
+func (a CountDowns) Len() int { return len(a) }
+func (a CountDowns) Less(i, j int) bool {
+	d1, _ := time.Parse("2006-01-02 15:04", a[i].Date)
+	d2, _ := time.Parse("2006-01-02 15:04", a[j].Date)
+	return d1.UnixNano() < d2.UnixNano()
+}
+func (a CountDowns) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
+func yamlpostHandler(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+
+	var cds CountDowns
+	for key, value := range req.Form {
+		// fmt.Println(key, value[0])
+		cd := CountDown{key, value[0]}
+		cds = append(cds, cd)
+	}
+	sort.Sort(CountDowns(cds))
+
+	var d CountDownData
+	d.CountDowns = cds
+
+	buf, err := yaml.Marshal(&d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("countdown.yaml", buf, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Redirect(w, req, "/yamledit", 301)
+}
+
 func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
@@ -72,6 +142,8 @@ func main() {
 	}
 
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources/"))))
+	http.HandleFunc("/yamlpost", yamlpostHandler)
+	http.HandleFunc("/yamledit", yamleditHandeler)
 	http.HandleFunc("/countdown", countdownHandeler)
 	http.HandleFunc("/", htmlHandler)
 
