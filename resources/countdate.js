2fe2d6e5228a27ab7ce5fbf33dd29b1bfc03cafd
shotahirama <vghjjj@gmail.com>
2019-01-25 18:59:23 +0900

    add タイムリミット過ぎた場合のやつ

--- resources/countdate.js
+++ resources/countdate.js
@@ -14,19 +14,20 @@ function countdate(limitdatestr){
       var addZero = function(n) {
         return ('0' + n).slice(-2);
       }
-      var addZero2 = function(n){
-        return ('0'+n).slice(-2);
-      }
       var gDate = function(rTime) {
-        var rDay = Math.floor(rTime / (60 * 60 * 24));
-        var rHour = Math.floor(rTime / (60 * 60)) - (rDay * 24);
-        var rMin = addZero(Math.floor(rTime / (60)) - (rDay * 24 * 60) - (rHour * 60));
-        var rSec = addZero(Math.floor(rTime) - (rDay * 24 * 60 * 60) - (rHour * 60 * 60) - (rMin * 60));
-        var rSec2 = addZero2(Math.floor((rTime-Math.floor(rTime))*100));
-        rDay = rDay ? rDay + '日' : '';
-        rHour = rHour ? rHour + '時間' : '';
-        rMin = rMin !== '00' ? rMin + '分' : '';
-        timelimit.textContent = rDay + rHour + rMin + rSec + '.'+rSec2+'秒';
+        if(rTime > 0.0){
+          var rDay = Math.floor(rTime / (60 * 60 * 24));
+          var rHour = Math.floor(rTime / (60 * 60)) - (rDay * 24);
+          var rMin = addZero(Math.floor(rTime / (60)) - (rDay * 24 * 60) - (rHour * 60));
+          var rSec = addZero(Math.floor(rTime) - (rDay * 24 * 60 * 60) - (rHour * 60 * 60) - (rMin * 60));
+          var rSec2 = addZero2(Math.floor((rTime-Math.floor(rTime))*100));
+          rDay = rDay ? rDay + '日' : '';
+          rHour = rHour ? rHour + '時間' : '';
+          rMin = rMin !== '00' ? rMin + '分' : '';
+          timelimit.textContent = rDay + rHour + rMin + rSec + '.'+rSec2+'秒';
+        }else{
+          timelimit.textContent = '期限が過ぎました';
+        }
       }
       gDate(rTime);
   },10);
